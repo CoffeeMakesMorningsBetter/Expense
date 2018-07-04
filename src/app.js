4af1afe3097416546aa42b6add01761ba82c9874
Mark Suzuki <markdsuzuki@gmail.com>
2018-07-04 13:02:50 -0700

    redux added

--- src/app.js
+++ src/app.js
@@ -1,8 +1,28 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import AppRouter from './routers/AppRouter'
+import configureStore from './store/configureStore';
+import { addExpense } from './actions/expenses';
+import { setTextFilter } from './actions/filters';
+import { getVisibleExpenses } from './selectors/expenses';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
+
+const store = configureStore();
+
+
+// addExpense -> Water bill
+store.dispatch(addExpense({description: 'Water Bill'}))
+// addExpense -> Gas bill
+store.dispatch(addExpense({description: 'Gas Bill'}))
+// setTextFilter -> bill
+store.dispatch(setTextFilter('bill'))
+
+// getVisibleExpenses
+const state = store.getState();
+const visbleExpenses = getVisibleExpenses(state.expenses, state.filters)
+console.log(visbleExpenses)
+
 
 
 ReactDOM.render(<AppRouter/>, document.getElementById('app'))
---
+++ src/reducers/filters.js
@@ -0,0 +1,44 @@
+
+const filterReducerDefaultState = {
+  text: '',
+  sortBy: 'date', // or amount
+  startDate: undefined,
+  endDate: undefined
+}
+
+// Filter Reducer 
+
+export const filterReducer = (state = filterReducerDefaultState, action) => {
+  switch(action.type){
+    case 'SET_TEXT_FILTERS':
+      console.log('SET_TEXT_FILTERS')
+      return {
+        ...state,
+        text: action.text
+      }
+    case 'SORT_BY_DATE':
+      console.log('SORT_BY_DATE')
+      return {
+        ...state,
+        sortBy: action.sortBy
+      }
+    case 'SORT_BY_AMOUNT':
+      console.log('SORT_BY_AMOUNT')
+      return {
+        ...state,
+        sortBy: action.sortBy
+      }
+    case 'SET_START_DATE':
+      return {
+        ...state,
+        startDate: action.startDate
+      }
+    case 'SET_END_DATE':
+    return {
+      ...state,
+      endDate: action.endDate
+    }
+    default:
+      return state;
+  }
+}
